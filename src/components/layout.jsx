f955a18b418558c13aa60d2753cb5c6495f444ac
Elue Michael <meekyberry6@gmail.com>
2022-11-05 08:31:34 +0100

    code update

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -40,27 +40,3 @@ const Layout = () => {
 }
 
 export default Layout;
-//       to={to}              // this is the path to which the link will navigate
-//       end         // this is a boolean that indicates whether the link should be active when the location is matched exactly or not
-//       {...props} />   // this is the props that will be passed to the NavLink component
-//   );
-// }
-
-
-// const Layout = () => {
-//   return (
-//     <div className="layout">
-//       <header className="app-header">
-//         <h1>RanDomUsersApp</h1>
-//       </header>
-//       <nav className="navigation">
-//         <CustomNavLink to="/" className="nav__item">Home</CustomNavLink>
-//         <CustomNavLink to="./users" className="nav__item">Users</CustomNavLink>
-//         <CustomNavLink to="about" className="nav__item">About Us</CustomNavLink>
-//         <CustomNavLink to="help" className="nav__item">How it works</CustomNavLink>
-//       </nav>
-//     </div>
-//   );
-// }
-
-// export default Layout;
